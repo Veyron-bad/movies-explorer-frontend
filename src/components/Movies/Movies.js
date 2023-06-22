7d16753c1f7c867f13a890a32a31d33e953838ee
Danilovich Iliya <danilovich-iliya@yandex.ru>
2023-06-23 02:02:49 +0700

    build: Создать интерфейс v.1

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
